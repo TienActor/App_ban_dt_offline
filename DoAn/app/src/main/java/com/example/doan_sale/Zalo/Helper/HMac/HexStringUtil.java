21bb7f20ca39acaa09406e0415dc50d4755ecf50
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-01 20:40:23 +0700

    Update HexStringUtil.java

--- DoAn/app/src/main/java/com/example/doan_sale/Zalo/Helper/HMac/HexStringUtil.java
+++ DoAn/app/src/main/java/com/example/doan_sale/Zalo/Helper/HMac/HexStringUtil.java
@@ -1,7 +1,5 @@
 package com.example.doan_sale.Zalo.Helper.HMac;
-
 import java.util.Locale;
-
 public class HexStringUtil {
     private static final byte[] HEX_CHAR_TABLE = {
             (byte) '0', (byte) '1', (byte) '2', (byte) '3',
@@ -9,16 +7,6 @@ public class HexStringUtil {
             (byte) '8', (byte) '9', (byte) 'a', (byte) 'b',
             (byte) 'c', (byte) 'd', (byte) 'e', (byte) 'f'
     };
-    // @formatter:on
-
-    /**
-     * Convert a byte array to a hexadecimal string
-     *
-     * @param raw
-     *            A raw byte array
-     *
-     * @return Hexadecimal string
-     */
     public static String byteArrayToHexString(byte[] raw) {
         byte[] hex = new byte[2 * raw.length];
         int index = 0;
@@ -30,15 +18,6 @@ public class HexStringUtil {
         }
         return new String(hex);
     }
-
-    /**
-     * Convert a hexadecimal string to a byte array
-     *
-     * @param hex
-     *            A hexadecimal string
-     *
-     * @return The byte array
-     */
     public static byte[] hexStringToByteArray(String hex) {
         String hexstandard = hex.toLowerCase(Locale.ENGLISH);
         int sz = hexstandard.length() / 2;
