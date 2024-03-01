f5201eaf0710f4f414da5a75039ba4e2d831bc60
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-01 20:47:22 +0700

    Update SearchResults.java

--- DoAn/app/src/main/java/com/example/doan_sale/ui/SearchResults.java
+++ DoAn/app/src/main/java/com/example/doan_sale/ui/SearchResults.java
@@ -1,18 +1,14 @@
 package com.example.doan_sale.ui;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
 import com.example.doan_sale.Product.ChiTietSanPhamActivity;
 import com.example.doan_sale.R;
 import com.example.doan_sale.model.Product;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,7 +33,6 @@ public class SearchResults extends AppCompatActivity  implements MainAdapter.Mai
             }
         });
         rvSearchResults.setAdapter(mainAdapter);
-
         // Lấy từ khóa từ Intent
         String query = getIntent().getStringExtra("SEARCH_QUERY");
         performSearch(query);
@@ -51,23 +46,19 @@ public class SearchResults extends AppCompatActivity  implements MainAdapter.Mai
     private void performSearch(String query) {
         // Giả sử bạn có một phương thức để tìm kiếm sản phẩm dựa trên từ khóa
         List<Product> results = searchInDatabase(query); // Thay thế bằng phương thức tìm kiếm của bạn
-
         // Cập nhật kết quả tìm kiếm
         searchResultsList.clear();
         searchResultsList.addAll(results);
         mainAdapter.notifyDataSetChanged();
     }
-
     private List<Product> searchInDatabase(String query) {
         // Thay thế phần này với logic tìm kiếm thực tế của bạn
         // Ví dụ: truy vấn cơ sở dữ liệu SQLite, hoặc sử dụng một lớp quản lý dữ liệu, v.v.
         DBHelper dbHelper = new DBHelper(this);
         return dbHelper.searchProducts(query);
     }
-
     @Override
     public void onItemClick(int id) {
-
     }
     private void onClickrecyclerview(Product product){
         Intent intent=new Intent(SearchResults.this, ChiTietSanPhamActivity.class);
