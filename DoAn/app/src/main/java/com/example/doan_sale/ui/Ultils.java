dff48835ed616fefea6ce0b112d08d07fa3fda59
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-01 20:47:48 +0700

    Update Ultils.java

--- DoAn/app/src/main/java/com/example/doan_sale/ui/Ultils.java
+++ DoAn/app/src/main/java/com/example/doan_sale/ui/Ultils.java
@@ -1,13 +1,10 @@
 package com.example.doan_sale.ui;
-
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
-
 public class Ultils {
     //Biến
     public static final String DATABASE_NAME="shop";
@@ -23,18 +20,12 @@ public class Ultils {
     public static final String USER_AVATAR="avatar";
     public static final String PRODUCT_ID = "product_id";
     public static final String PRODUCT_NAME="name";
-
     public static final String PRODUCT_PRICE="price";
-
     public static final String PRODUCT_IMAGE="image";
-
     public static final String PRODUCT_DESCRIPTION="description";
-
     public static final String CATEGORY_ID="cateid";
     public static final String CATEGORY_NAME="name";
-
     public static final String ORDER_ADDRESS="address";
-
     public static final String ORDER_ID="orderid";
     public static final String QUANTITY="quantity";
     public static final String PRICE="price";
@@ -45,22 +36,17 @@ public class Ultils {
     public static final String VOUCHER_DISCOUNT = "discount";
     public static final String VOUCHER_START_DATE = "start_date";
     public static final String VOUCHER_END_DATE = "end_date";
-
     //TABLE
     public static final String TABLE_USER="user";
     public static final String TABLE_ADMIN="admin";
     public static final String TABLE_CATEGORY="category";
     public static final String TABLE_PRODUCT="product";
-
     public static final String TABLE_VOUCHER = "voucher";
     public static final String PAYMENT_METHOD = "payment_method";
     public static final String TABLE_ORDER = "orders"; // Thay vì "order"
-
-
     public static final String ORDER_DATE = "date";
     public static final String ORDER_STATUS = "status";
     public static final String ORDER_TOTAL = "total";
-
     public static final String TABLE_ORDER_DETAIL = "order_detail";
     public static final String ORDER_DETAIL_ID = "order_detail_id";
     public static Bitmap convertToBitmapFromAssets(Context context, String nameImage){
