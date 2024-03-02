f40bcdf3268007d86b8430f6ace2df89adc3df39
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-02 22:59:08 +0700

    Update Order.java

--- DoAn/app/src/main/java/com/example/doan_sale/model/Order.java
+++ DoAn/app/src/main/java/com/example/doan_sale/model/Order.java
@@ -5,15 +5,12 @@ public class Order {
     private String date;
     private String address;
     private double total;
-
     public Order(int orderId, String date, String address, double total) {
         this.orderId = orderId;
         this.date = date;
         this.address = address;
         this.total = total;
     }
-
-
     // Getters và Setters
     public int getOrderId() { return orderId; }
     public void setOrderId(int orderId) { this.orderId = orderId; }
@@ -21,11 +18,9 @@ public class Order {
     public void setDate(String date) { this.date = date; }
     public double getTotal() { return total; }
     public void setTotal(double total) { this.total = total; }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
