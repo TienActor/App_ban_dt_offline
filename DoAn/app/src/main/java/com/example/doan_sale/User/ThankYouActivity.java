2b18784d9a5feaf4a646f072f44ea5782eb81bef
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-04 01:54:08 +0700

    Update ThankYouActivity.java

--- DoAn/app/src/main/java/com/example/doan_sale/User/ThankYouActivity.java
+++ DoAn/app/src/main/java/com/example/doan_sale/User/ThankYouActivity.java
@@ -1,15 +1,11 @@
 package com.example.doan_sale.User;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.doan_sale.R;
 import com.example.doan_sale.ui.MainActivity;
-
 public class ThankYouActivity extends AppCompatActivity {
 Button hoanthanh;
     @Override
