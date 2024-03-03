c314dcff8c0b8e848ac31da594588edccee772af
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-03 13:08:37 +0700

    Update LogOutFragment.java

--- DoAn/app/src/main/java/com/example/doan_sale/fragment/LogOutFragment.java
+++ DoAn/app/src/main/java/com/example/doan_sale/fragment/LogOutFragment.java
@@ -1,20 +1,15 @@
 package com.example.doan_sale.fragment;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.example.doan_sale.Account.LoginActivity;
 import com.example.doan_sale.R;
 import com.example.doan_sale.ui.MainActivity;
-
 
 public class LogOutFragment extends Fragment {
     private Button btnConfirm;
@@ -24,25 +19,20 @@ public class LogOutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_log_out, container, false);
-
         btnConfirm = view.findViewById(R.id.btn_logout_confirm);
         btnCancel = view.findViewById(R.id.btn_logout_cancel);
-
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Xóa thông tin đăng nhập khỏi SharedPreferences
                 logout();
-
                 // Chuyển đến LoginActivity
                 Intent i=new Intent(getActivity(), MainActivity.class);
                 startActivity(i);
-
                 // Đóng Fragment hiện tại
                 getFragmentManager().popBackStack();
             }
         });
-
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
