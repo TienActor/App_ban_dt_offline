ee687e1eddf78026277305e98a2dba7053e84727
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-03 13:09:39 +0700

    Update VoucherFragment.java

--- DoAn/app/src/main/java/com/example/doan_sale/fragment/VoucherFragment.java
+++ DoAn/app/src/main/java/com/example/doan_sale/fragment/VoucherFragment.java
@@ -1,22 +1,17 @@
 package com.example.doan_sale.fragment;
-
 import static java.nio.file.Files.size;
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.example.doan_sale.Admin.AdminActivity;
 import com.example.doan_sale.Admin.VoucherAdapter;
 import com.example.doan_sale.Admin.VoucherListActivity;
@@ -24,23 +19,15 @@ import com.example.doan_sale.R;
 import com.example.doan_sale.model.Voucher;
 import com.example.doan_sale.ui.DBHelper;
 import com.example.doan_sale.ui.Ultils;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class VoucherFragment extends Fragment {
-
     private RecyclerView recyclerView;
     private VoucherAdapter adapter;
     private List<Voucher> voucherList;
     private DBHelper dbHelper;
     private Button back;
-
-
-
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,17 +44,9 @@ public class VoucherFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity())); // Use getActivity() here
         adapter = new VoucherAdapter(voucherList);
         recyclerView.setAdapter(adapter);
-
-
-
         loadVoucherList();
         return view; // Return the inflated view
     }
-
-
-
-
-
     private void loadVoucherList() {
         voucherList.clear();
         SQLiteDatabase db = dbHelper.getReadableDatabase();
