8aae1b9cb819b77e0d7eb17f1af8a657c0ae6bb6
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-09 21:35:11 +0700

    Update LSDHFragment.java

--- DoAn/app/src/main/java/com/example/doan_sale/LSDH/LSDHFragment.java
+++ DoAn/app/src/main/java/com/example/doan_sale/LSDH/LSDHFragment.java
@@ -1,5 +1,4 @@
 package com.example.doan_sale.LSDH;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,51 +7,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.doan_sale.R;
 import com.example.doan_sale.model.Order;
 import com.example.doan_sale.ui.DBHelper;
 import com.example.doan_sale.ui.MainActivity;
-
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
 public class LSDHFragment extends Fragment {
     private RecyclerView recyclerView;
     private LSDHAdapter adapter;
     private List<Order> orderList;
     private DBHelper dbHelper;
     private ImageButton gobackbtn;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_l_s_d_h, container, false);
-
         gobackbtn = view.findViewById(R.id.gobackbtn);
         recyclerView = view.findViewById(R.id.fragmentLSDH);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         dbHelper = new DBHelper(getActivity());
-
         gobackbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().onBackPressed();
             }
         });
-
         loadOrderData();
-
         adapter = new LSDHAdapter(getActivity(), orderList);
         recyclerView.setAdapter(adapter);
-
         return view;
     }
-
     private void loadOrderData() {
         String username = getUsernameFromSharedPreferences();
         if (username != null) {
