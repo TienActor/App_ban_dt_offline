3d2faae38e68bcd77bb3326f20277e02b7e0ffed
Tiến nguyễn <134863539+TienActor@users.noreply.github.com>
2024-03-15 14:34:49 +0700

    Update UserAdapter.java

--- DoAn/app/src/main/java/com/example/doan_sale/Admin/UserAdapter.java
+++ DoAn/app/src/main/java/com/example/doan_sale/Admin/UserAdapter.java
@@ -1,23 +1,18 @@
 package com.example.doan_sale.Admin;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-
 import com.example.doan_sale.Account.RegisterActivity;
 import com.example.doan_sale.R;
 import com.example.doan_sale.model.user;
 import com.example.doan_sale.ui.MainActivity;
 import com.example.doan_sale.ui.Ultils;
 import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder>{
